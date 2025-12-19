7265705048fb95019a0c609fa59aa876dcf9d3fe
Safori454 <gottfriedsarfo@gmail.com>
2025-12-19 16:50:48 +0000

    Database Config

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-//   ssl: {
-//     rejectUnauthorized: false 
-//   }
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
 // Test DB connection
